ed55b6c28c0dcf068ca34bd6fabfa328b32c13db
Olivier Andriessen <olivierandriessen@gmail.com>
2015-06-10 22:44:54 +0200

    Fix postman export request data

--- src/PhalconRest/Documentation/Generator.php
+++ src/PhalconRest/Documentation/Generator.php
@@ -2,128 +2,124 @@
 
 namespace PhalconRest\Documentation;
 
-use League\Fractal\Resource\Collection,
-	League\Fractal\Resource\Item;
+class Generator extends \Phalcon\Mvc\User\Plugin
+{
 
-class Generator extends \Phalcon\Mvc\User\Plugin {
+    protected $reader;
+    protected $collections;
+    protected $collectionClasses = [];
+    protected $handlers = [];
 
-	protected $reader;
-	protected $collections;
-	protected $collectionClasses = [];
-	protected $handlers = [];
+    public function __construct($collections = [])
+    {
+        $this->collections = $collections;
+        $this->reader = new \Phalcon\Annotations\Adapter\Memory();
+    }
 
-	public function __construct($collections = [])
-	{
-		$this->collections = $collections;
-		$this->reader = new \Phalcon\Annotations\Adapter\Memory();
-	}
+    protected function getCollectionClasses()
+    {
 
-	protected function getCollectionClasses()
-	{
+        if (empty($this->collectionClasses)) {
 
-		if (empty($this->collectionClasses)) {
+            foreach ($this->collections as $collection) {
 
-			foreach ($this->collections as $collection){
+                $this->collectionClasses[] = new $collection;
+            }
+        }
 
-				$this->collectionClasses[] = new $collection;
-			}
-		}
+        return $this->collectionClasses;
+    }
 
-		return $this->collectionClasses;
-	}
+    protected function getAnnotationsFromCollection($collection)
+    {
+        $handler = $collection->getHandler();
+        $reflector = $this->reader->get($handler);
+        return [
+            "class" => $reflector->getClassAnnotations(),
+            "methods" => $reflector->getMethodsAnnotations(),
+        ];
+    }
 
-	protected function getAnnotationsFromCollection($collection)
-	{
-		$handler = $collection->getHandler();
-		$reflector = $this->reader->get($handler);
-		return [
-			"class" => $reflector->getClassAnnotations(),
-			"methods" => $reflector->getMethodsAnnotations()
-		];
-	}
+    protected function getAnnotationsFromCollections()
+    {
 
-	protected function getAnnotationsFromCollections()
-	{
+        $data = [];
 
-		$data = [];
+        foreach ($this->getCollectionClasses() as $collection) {
 
-		foreach ($this->getCollectionClasses() as $collection){
+            $handler = $collection->getHandler();
+            $annotations = $this->getAnnotationsFromCollection($collection);
+            $classAnnotations = $annotations["class"];
 
-			$handler = $collection->getHandler();
-			$annotations = $this->getAnnotationsFromCollection($collection);
-			$classAnnotations = $annotations["class"];
+            $resource = "Untitled Resource - Specify @resource above class";
 
-			$resource = "Untitled Resource - Specify @resource above class";
+            if (is_object($classAnnotations) && $classAnnotations->has("resource")) {
 
-			if (is_object($classAnnotations) && $classAnnotations->has("resource")){
+                $resource = $classAnnotations->get("resource")->getArgument(0);
+            }
 
-				$resource = $classAnnotations->get("resource")->getArgument(0);
-			}
+            $data[$handler]["resource"] = $resource;
+            $data[$handler]["collection"] = $collection;
+            $data[$handler]["annotations"] = $annotations["methods"];
+        }
 
-			$data[$handler]["resource"] = $resource;
-			$data[$handler]["collection"] = $collection;
-			$data[$handler]["annotations"] = $annotations["methods"];
-		}
+        return $data;
+    }
 
-		return $data;
-	}
+    protected function getDocumentedRoutesFromCollections()
+    {
 
-	protected function getDocumentedRoutesFromCollections()
-	{
+        $collections = $this->getAnnotationsFromCollections();
 
-		$collections = $this->getAnnotationsFromCollections();
+        foreach ($collections as $collection) {
 
-		foreach($collections as $collection){
+            $data[] = new Resource($collection["resource"], $collection["collection"], $collection["annotations"]);
+        }
 
-			$data[] = new Resource($collection["resource"], $collection["collection"], $collection["annotations"]);
-		}
+        return $data;
+    }
 
-		return $data;
-	}
+    public function generate()
+    {
+        return $this->getDocumentedRoutesFromCollections();
+    }
 
-	public function generate()
-	{
-		return $this->getDocumentedRoutesFromCollections();
-	}
+    public function generatePostmanCollection($hostName = 'http://no-hostname-defined.local/')
+    {
+        $resources = $this->getDocumentedRoutesFromCollections();
 
-	public function generatePostmanCollection($hostName = 'http://no-hostname-defined.local/')
-	{
-		$resources = $this->getDocumentedRoutesFromCollections();
+        $requests = [];
+        $collectionId = uniqid();
 
-		$requests = [];
-		$collectionId = uniqid();
+        foreach ($resources as $resource) {
 
-		foreach($resources as $resource)
-		{
+            foreach ($resource->endpoints as $endpoint) {
+                $data = null;
 
-			foreach($resource->endpoints as $endpoint)
-			{
-				$data = "";
+                if (is_object($endpoint->requestExample) || is_array($endpoint->requestExample)) {
 
-				if (is_object($endpoint->requestExample) || is_array($endpoint->requestExample)) {
+                    $data = $endpoint->requestExample;
+                }
 
-					$data = $endpoint->requestExample;
-				}
+                $request = new \stdClass;
+                $request->collectionId = $collectionId;
+                $request->id = uniqid();
+                $request->name = $endpoint->resource . ' - ' . $endpoint->title;
+                $request->description = $endpoint->description;
+                $request->url = $hostName . $endpoint->route;
+                $request->method = $endpoint->method;
+                $request->headers = "Authorization: Basic {{authToken}}";
+                $request->data = $data ? json_encode($data) : null;
+                $request->dataMode = "raw";
+                $requests[] = $request;
+            }
+        }
 
-				$request = new \stdClass;
-				$request->collectionId = $collectionId;
-				$request->id = uniqid();
-				$request->name = $endpoint->resource . ' - ' . $endpoint->title;
-				$request->description = $endpoint->description;
-				$request->url = $hostName . $endpoint->route;
-				$request->method = $endpoint->method;
-				$request->headers = "Authorization: Basic {{authToken}}";
-				$request->data = $data;
-				$request->dataMode = "raw";
-				$requests[] = $request;
-			}
-		}
+        $collection = new \stdClass;
+        $collection->id = $collectionId;
+        $collection->name = $hostName;
+        $collection->requests = $requests;
 
-		$collection = new \stdClass;
-		$collection->id = $collectionId;
-		$collection->name = $hostName;
-		$collection->requests = $requests;
-
-		return $collection;
-	}
+        return $collection;
+    }
 }
