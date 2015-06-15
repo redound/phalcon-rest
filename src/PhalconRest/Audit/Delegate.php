6c75d6693c06834a10287462959b0541ec813e2c
Olivier Andriessen <olivierandriessen@gmail.com>
2015-06-15 13:07:30 +0200

    Restructure AuditManager

--- src/PhalconRest/Audit/Delegate.php
+++ src/PhalconRest/Audit/Delegate.php
@@ -4,5 +4,15 @@ namespace PhalconRest\Audit;
 
 class Delegate
 {
+    public function verify($name, $method, $data)
+    {
+        $event = new Event;
 
+        if (method_exists($this, $event)) {
+
+            call_user_method_array($method, $this, [$event, $data]);
+        }
+
+        return $event;
+    }
 }
--- src/PhalconRest/Audit/Manager.php
+++ src/PhalconRest/Audit/Manager.php
@@ -11,41 +11,19 @@ class Manager
         $this->delegates = [];
     }
 
-    public function mount($name, $instance)
-    {
-        return $this->setDelegate($name, $instance);
-    }
-
-    protected function setDelegate($name, Delegate $instance)
+    public function setDelegate($name, Delegate $instance)
     {
         $this->delegates[$name] = $instance;
 
         return $this;
     }
 
-    protected function getDelegate($name)
+    public function getDelegate($name)
     {
         if (!array_key_exists($name, $this->delegates)) {
             return false;
         }
 
         return $this->delegates[$name];
-    }
-
-    public function check($name, $event, $data)
-    {
-        $check = new Event;
-
-        if ($delegate = $this->getDelegate($name)) {
-
-            if (!method_exists($delegate, $event)) {
-
-                return $check;
-            }
-
-            call_user_method_array($event, $delegate, [$check, $data]);
-        }
-
-        return $check;
     }
 }
