8eadb0b9192752c1b07d641ae7240e5dc8fc1ea9
Bart Blok <bart@wittig.nl>
2015-11-06 14:32:58 +0100

    Shortcuts for services

--- src/PhalconRest/Constants/Services.php
+++ src/PhalconRest/Constants/Services.php
@@ -7,7 +7,7 @@ class Services
     const REQUEST = 'request';
     const RESPONSE = 'response';
 
-    const AUTH_MANAGER = 'phalcon_rest_auth_manager';
-    const FRACTAL_MANAGER = 'phalcon_rest_fractal_manager';
-    const TOKEN_PARSER = 'phalcon_rest_token_parser';
+    const AUTH_MANAGER = 'authManager';
+    const FRACTAL_MANAGER = 'fractalManager';
+    const TOKEN_PARSER = 'tokenParser';
 }
