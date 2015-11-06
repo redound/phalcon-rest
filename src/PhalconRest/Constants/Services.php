146958fbeafbf5693c2220dec6666501e78b5272
Bart Blok <bart@wittig.nl>
2015-11-06 15:32:13 +0100

    Added service constants for phalcon services

--- src/PhalconRest/Constants/Services.php
+++ src/PhalconRest/Constants/Services.php
@@ -4,9 +4,32 @@ namespace PhalconRest\Constants;
 
 class Services
 {
-    const REQUEST = 'request';
-    const RESPONSE = 'response';
+    // Phalcon
+    const DISPATCHER = "dispatcher";
+    const ROUTER = "router";
+    const URL = "url";
+    const REQUEST = "request";
+    const RESPONSE = "response";
+    const COOKIES = "cookies";
+    const FILTER = "filter";
+    const FLASH = "flash";
+    const FLASH_SESSION = "flashSession";
+    const SESSION = "session";
+    const EVENTS_MANAGER = "eventsManager";
+    const DB = "db";
+    const SECURITY = "security";
+    const CRYPT = "crypt";
+    const TAG = "tag";
+    const ESCAPER = "escaper";
+    const ANNOTATIONS = "annotations";
+    const MODELS_MANAGER = "modelsManager";
+    const MODELS_METADATA = "modelsMetadata";
+    const TRANSACTION_MANAGER = "transactionManager";
+    const MODELS_CACHE = "modelsCache";
+    const VIEWS_CACHE = "viewsCache";
+    const ASSETS = "assets";
 
+    // PhalconRest
     const AUTH_MANAGER = 'authManager';
     const FRACTAL_MANAGER = 'fractalManager';
     const TOKEN_PARSER = 'tokenParser';
